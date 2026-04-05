d0627eaae6135cf86bfcf6ee58c6b72269f22f44
arishprojects <arish@boteco.co.in>
2026-04-05 22:34:36 +0530

    fix: correct indentation in app.py

--- app.py
+++ app.py
@@ -59,71 +59,69 @@ st.markdown(styles.get_css(), unsafe_allow_html=True)
 auth.init_auth_state()
 
 if not auth.check_authentication():
-        auth.show_login_form()
-    else:
-        # Sidebar branding
-        st.sidebar.image("logo.png", width=180)
-        # Render sidebar
-        auth.render_auth_sidebar()
-        st.sidebar.divider()
-        st.sidebar.markdown("##### POS import")
+    auth.show_login_form()
+else:
+    # Sidebar branding
+    st.sidebar.image("logo.png", width=180)
+    # Render sidebar
+    auth.render_auth_sidebar()
+    st.sidebar.divider()
+    st.sidebar.markdown("##### POS import")
 
-        report_loc_ids = auth.get_report_location_ids()
-        report_display_name = auth.get_report_display_name()
-        all_locs = database.get_all_locations()
-        if len(all_locs) > 1 and auth.is_admin():
-            imp_labels = {
-                str(loc["id"]): loc["name"]
-                for loc in sorted(all_locs, key=lambda x: x["name"])
-            }
-            imp_keys = list(imp_labels.keys())
-            default_imp = str(st.session_state.location_id)
-            if default_imp not in imp_keys:
-                default_imp = imp_keys[0]
-            import_loc_id = int(
-                st.sidebar.selectbox(
-                    "POS import for",
-                    options=imp_keys,
-                    index=imp_keys.index(default_imp),
-                    format_func=lambda k: imp_labels[k],
-                    key="sidebar_import_location",
-                )
+    report_loc_ids = auth.get_report_location_ids()
+    report_display_name = auth.get_report_display_name()
+    all_locs = database.get_all_locations()
+    if len(all_locs) > 1 and auth.is_admin():
+        imp_labels = {
+            str(loc["id"]): loc["name"]
+            for loc in sorted(all_locs, key=lambda x: x["name"])
+        }
+        imp_keys = list(imp_labels.keys())
+        default_imp = str(st.session_state.location_id)
+        if default_imp not in imp_keys:
+            default_imp = imp_keys[0]
+        import_loc_id = int(
+            st.sidebar.selectbox(
+                "POS import for",
+                options=imp_keys,
+                index=imp_keys.index(default_imp),
+                format_func=lambda k: imp_labels[k],
+                key="sidebar_import_location",
             )
-        elif len(all_locs) > 1:
-            import_loc_id = int(st.session_state.location_id)
-            imp_name = st.session_state.location_name or "your location"
-            for loc in all_locs:
-                if loc["id"] == import_loc_id:
-                    imp_name = loc["name"]
-                    break
-            st.sidebar.caption(f"POS imports save to **{imp_name}**.")
-        else:
-            import_loc_id = (
-                all_locs[0]["id"] if all_locs else st.session_state.location_id
-            )
-
-        import_location_settings = database.get_location_settings(import_loc_id)
-        location_id = st.session_state.location_id
-
-        # Build shared context
-        ctx = TabContext(
-            location_id=location_id,
-            import_loc_id=import_loc_id,
-            report_loc_ids=report_loc_ids,
-            report_display_name=report_display_name,
-            all_locs=all_locs,
-            location_settings=database.get_location_settings(location_id),
-            import_location_settings=import_location_settings,
         )
+    elif len(all_locs) > 1:
+        import_loc_id = int(st.session_state.location_id)
+        imp_name = st.session_state.location_name or "your location"
+        for loc in all_locs:
+            if loc["id"] == import_loc_id:
+                imp_name = loc["name"]
+                break
+        st.sidebar.caption(f"POS imports save to **{imp_name}**.")
+    else:
+        import_loc_id = all_locs[0]["id"] if all_locs else st.session_state.location_id
 
-        # Tabs
-        tab1, tab2, tab3, tab4 = st.tabs(["Upload", "Report", "Analytics", "Settings"])
+    import_location_settings = database.get_location_settings(import_loc_id)
+    location_id = st.session_state.location_id
 
-        with tab1:
-            render_upload(ctx)
-        with tab2:
-            render_report(ctx)
-        with tab3:
-            render_analytics(ctx)
-        with tab4:
-            render_settings(ctx)
+    # Build shared context
+    ctx = TabContext(
+        location_id=location_id,
+        import_loc_id=import_loc_id,
+        report_loc_ids=report_loc_ids,
+        report_display_name=report_display_name,
+        all_locs=all_locs,
+        location_settings=database.get_location_settings(location_id),
+        import_location_settings=import_location_settings,
+    )
+
+    # Tabs
+    tab1, tab2, tab3, tab4 = st.tabs(["Upload", "Report", "Analytics", "Settings"])
+
+    with tab1:
+        render_upload(ctx)
+    with tab2:
+        render_report(ctx)
+    with tab3:
+        render_analytics(ctx)
+    with tab4:
+        render_settings(ctx)
