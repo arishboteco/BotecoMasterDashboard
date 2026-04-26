ede95ad0df5774bb591d76e1ecdeb206f8f33e81
arishprojects <arish@boteco.co.in>
2026-04-26 12:35:44 +0530

    Fix December month-length calculation and add coverage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,3 +9,11 @@ class TestFormatPercent:
 
     def test_handles_none_as_zero(self):
         assert utils.format_percent(None) == "0.00%"
+
+
+class TestGetDaysInMonth:
+    def test_returns_31_for_december(self):
+        assert utils.get_days_in_month(2025, 12) == 31
+
+    def test_returns_29_for_leap_february(self):
+        assert utils.get_days_in_month(2024, 2) == 29
